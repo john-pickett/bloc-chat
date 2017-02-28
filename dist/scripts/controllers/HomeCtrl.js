baecaab61eadbaba9be7053bbe7da24967364ac0
John P <john.pickett@gmail.com>
2017-02-28 10:19:17 -0600

    removed chat view and added styles

--- dist/scripts/controllers/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    function HomeCtrl() {
-        this.content = "Here's some content for the home page!";
-    }
-    
-    angular
-        .module('blocChat')
-        .controller('HomeCtrl', HomeCtrl);
-})();
