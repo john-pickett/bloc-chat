fbf11cce5b31b953ec16e3415855486b3212c484
John P <john.pickett@gmail.com>
2017-03-02 10:55:28 -0600

    username works

---
+++ app/scripts/Cookies.js
@@ -0,0 +1,43 @@
+(function() {
+  function BlocChatCookies($cookies, $uibModal){
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (currentUser) {
+      console.log(currentUser);
+    } else {
+      console.log("No user set");
+    }
+    if (!currentUser || currentUser === "") {
+      // toggle modal to set username
+      // $scope.newChatRoom = function() {
+
+        var modalInstance = $uibModal.open({
+            templateUrl: '/templates/user-modal.html',
+            controller: function($scope, $uibModalInstance) {
+                // this.newRoom = {};
+                $scope.cancel = function() {
+                    // modalInstance.dismiss('Cancel');
+                    $uibModalInstance.dismiss("Cancel");
+                };
+
+                $scope.create = function() {
+                      // need something else here for real function
+                    // modalInstance.close($scope.name);
+                    $uibModalInstance.close($scope.newUser);
+                };
+            },
+            size: 'md',
+        });
+
+        modalInstance.result.then(function(data){
+          $cookies.put('blocChatCurrentUser', data);
+        });
+
+      } // end of if statement
+    }
+
+
+
+  angular
+    .module('blocChat')
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
+})();
